d792e31c24c770c3f69665a7f988fc3baaddf21c
Ronnie <bourdear@gmail.com>
2021-06-10 19:05:01 -0700

    Change out API

--- src/App.js
+++ src/App.js
@@ -6,35 +6,32 @@ import React, {useState} from 'react'
 import axios from 'axios'
 import moment from 'moment'
 import apiKey from './openWeatherKey'
-import locationKey from './locationIqKey'
+
 
 function App() {
   const [search, setSearch] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
   const [apiData, setApiData] = useState([])
-  const [locationData, setLocationData] = useState([])
+  const [nextWeekData, setNextWeekData] = useState([])
   const [showData, setShowData] = useState(false)
   const [windDirection, setWindDirection] = useState('')
   const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
 
   const getData = async () => {
-    const locationURL = `https://us1.locationiq.com/v1/search.php?key=${locationKey}&q=${searchTerm}&countrycodes=us&format=json`
-    const response = await axios.get(locationURL)
-    setLocationData(response.data)
-    const lat = response.data[0].lat
-    const lon = response.data[0].lon
-    const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-    const weatherInfo = await axios.get(apiURL)
-    setApiData(() => weatherInfo.data)
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}&units=imperial`
+    const currentWeatherInfo = await axios.get(apiURL)
+    const lat = currentWeatherInfo.data.coord.lat
+    const lon = currentWeatherInfo.data.coord.lon
+    const nextWeekApiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly&appid=${apiKey}&units=imperial`
+    const nextWeekWeather = await axios.get(nextWeekApiURL)
+    setApiData(() => currentWeatherInfo.data)
+    setNextWeekData(() => nextWeekWeather.data)
     setShowData(() => true)
-    console.log('data Fetched')
-    // .catch(error => {
-    //   console.log("That didn't work")
-    }
+  }
 
   const handleSearch = (event) => {
-    setSearch(event.target.value)
-    setSearchTerm(search.split(' ').join('%20'))
+    setSearch(() => event.target.value)
+    setSearchTerm(() => event.target.value.split(' ').join('%20'))
   }
 
   const handleClick = (event) => {
@@ -53,8 +50,6 @@ function App() {
     return str.charAt(0).toUpperCase() + str.slice(1)
   }
 
-  console.log(apiData)
-
   return (
     <div className="App">
       <h1>How's the Weather?</h1>
@@ -66,63 +61,65 @@ function App() {
       </div> {showData &&
         <div>
           <CurrentWeather
-            dateTime={moment.unix(apiData.current.dt).format('LLL')}
-            name={locationData[0].display_name} 
-            desc={capFirstLetter(apiData.current.weather[0].description)}
-            temp={Math.round(apiData.current.temp)}
-            feelsLike={Math.round(apiData.current.feels_like)}
-            sunrise={moment.unix(apiData.current.sunrise).format('LT')}
-            sunset={moment.unix(apiData.current.sunset).format('LT')} 
+            dateTime={moment.unix(apiData.dt).format('LLL')}
+            name={apiData.name} 
+            country={apiData.sys.country}
+            icon={`http://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png`}
+            desc={capFirstLetter(apiData.weather[0].description)}
+            temp={Math.round(apiData.main.temp)}
+            feelsLike={Math.round(apiData.main.feels_like)}
+            sunrise={moment.unix(apiData.sys.sunrise).utcOffset((apiData.timezone) / 60).format('LT')}
+            sunset={moment.unix(apiData.sys.sunset).utcOffset((apiData.timezone) / 60).format('LT')} 
           />
           <h2>7-day forecast</h2> 
           <div className="next-week-parent">
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[0].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[0].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[0].temp.max)} low={Math.round(apiData.daily[0].temp.min)}
-              desc={apiData.daily[0].weather[0].description}
+              date={moment.unix(nextWeekData.daily[1].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[1].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[1].temp.max)} low={Math.round(nextWeekData.daily[1].temp.min)}
+              desc={nextWeekData.daily[1].weather[0].description}
             />  
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[1].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[1].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[1].temp.max)} low={Math.round(apiData.daily[1].temp.min)}
-              desc={apiData.daily[1].weather[0].description}
-            /> 
+              date={moment.unix(nextWeekData.daily[2].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[2].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[2].temp.max)} low={Math.round(nextWeekData.daily[2].temp.min)}
+              desc={nextWeekData.daily[2].weather[0].description}
+            />  
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[2].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[2].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[2].temp.max)} low={Math.round(apiData.daily[2].temp.min)}
-              desc={apiData.daily[2].weather[0].description}
-            /> 
+              date={moment.unix(nextWeekData.daily[3].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[3].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[3].temp.max)} low={Math.round(nextWeekData.daily[3].temp.min)}
+              desc={nextWeekData.daily[3].weather[0].description}
+            />  
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[3].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[3].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[3].temp.max)} low={Math.round(apiData.daily[3].temp.min)}
-              desc={apiData.daily[3].weather[0].description}
-            /> 
+              date={moment.unix(nextWeekData.daily[4].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[4].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[4].temp.max)} low={Math.round(nextWeekData.daily[4].temp.min)}
+              desc={nextWeekData.daily[4].weather[0].description}
+            />  
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[4].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[4].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[4].temp.max)} low={Math.round(apiData.daily[4].temp.min)}
-              desc={apiData.daily[4].weather[0].description}
-            /> 
+              date={moment.unix(nextWeekData.daily[5].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[5].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[5].temp.max)} low={Math.round(nextWeekData.daily[5].temp.min)}
+              desc={nextWeekData.daily[5].weather[0].description}
+            />  
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[5].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[5].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[5].temp.max)} low={Math.round(apiData.daily[5].temp.min)}
-              desc={apiData.daily[5].weather[0].description}
-            /> 
+              date={moment.unix(nextWeekData.daily[6].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[6].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[6].temp.max)} low={Math.round(nextWeekData.daily[6].temp.min)}
+              desc={nextWeekData.daily[6].weather[0].description}
+            />  
             <WeatherThisWeek 
-              date={moment.unix(apiData.daily[6].dt).format('ddd, MMM DD')}
-              icon={`http://openweathermap.org/img/wn/${apiData.daily[6].weather[0].icon}@2x.png`}
-              high={Math.round(apiData.daily[6].temp.max)} low={Math.round(apiData.daily[6].temp.min)}
-              desc={apiData.daily[6].weather[0].description}
-            /> 
+              date={moment.unix(nextWeekData.daily[7].dt).format('ddd, MMM DD')}
+              icon={`http://openweathermap.org/img/wn/${nextWeekData.daily[7].weather[0].icon}@2x.png`}
+              high={Math.round(nextWeekData.daily[7].temp.max)} low={Math.round(nextWeekData.daily[7].temp.min)}
+              desc={nextWeekData.daily[7].weather[0].description}
+            />  
           </div>     
         </div>
       }  
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const CurrentWeather = ({ dateTime, name, desc, temp, feelsLike, sunrise, sunset }) => {
+const CurrentWeather = ({ dateTime, name, country, icon, desc, temp, feelsLike, sunrise, sunset }) => {
   return (
     <div>
       <p>{dateTime}</p>
-      <p>Current Weather in {name}</p>
+      <p>Current Weather in {name}, {country}</p>
+      <img src={icon} alt="Weather icon."/>
       <h2>{temp}° F</h2>
       <p>Feels like {feelsLike}°F. {desc}.</p>
       <p>Sunrise: {sunrise} Sunset: {sunset}</p>
